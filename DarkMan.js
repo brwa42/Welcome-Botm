5a0d5242842a757fcdc522004d96008f07f3caff
brwa42 <79860367+brwa42@users.noreply.github.com>
2021-10-14 15:48:39 +0300

    Update DarkMan.js

--- DarkMan.js
+++ DarkMan.js
@@ -25,7 +25,7 @@ const DarkMan = new Discord.Client();
 const prefix = "D!";
 
 
-DarkMan.login("");
+DarkMan.login("ODk4MTQzMzcwMTM5MDI5NTI0.YWf7Mg.IEYGS_h1YtsoVeQr2_PnaTuhZ48");
 DarkMan.on("ready", async () => {
   console.log(`Logged in as ${DarkMan.user.username}!`);
   DarkMan.user.setStatus("ONLINE");
@@ -46,8 +46,8 @@ const embed = new Discord.MessageEmbed()
 .setAuthor("Prefix [ D! ]","")
 .setDescription(` 
 **
-・┊welcome [change channel]
-・┊left [change channel]
+・┊welcome [welcome]
+・┊left [reklam]
 **
 **[ invite ]**
 **[ click here ](https://discord.com/api/oauth2/authorize?client_id=743869029638471802&permissions=8&scope=bot)** 
